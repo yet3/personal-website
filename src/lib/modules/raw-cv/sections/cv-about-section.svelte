62e98602fc1620fb311a02365b9a94fce2df57e1
Max Kasperowicz <yet3.dev@gmail.com>
2025-10-05 20:47:08 +0200

    feat: align cv about section with page's

--- src/lib/modules/raw-cv/sections/cv-about-section.svelte
+++ src/lib/modules/raw-cv/sections/cv-about-section.svelte
@@ -5,7 +5,7 @@
 <CvSection title="About me">
 	<p class="text-[12.5px] text-justify">
 		I'm a full-stack developer from Poland who has a passion for building highly interactive
-		applications, whether they're useful tools or just for fun pet projects. Outside of programming,
+		applications, whether they're useful tools or just fun projects. Outside of programming,
 		I love reading sci-fi, dystopian, and fantasy genres. I’m also a coffee and tea enthusiast,
 		always eager to try new tastes.
 	</p>
