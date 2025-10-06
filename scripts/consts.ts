aa576e70dbc557d7bcc73232381047712d06427d
Max Kasperowicz <yet3.dev@gmail.com>
2025-10-07 00:16:51 +0200

    feat: add vercel specific output path

--- scripts/consts.ts
+++ scripts/consts.ts
@@ -1,5 +1,8 @@
 import { join } from "path";
 
-export const BUILD_DIR = join(process.cwd(), "build");
+export const BUILD_DIR = join(
+	process.cwd(),
+	import.meta.env.VERCEL ? ".vercel/output/static" : "build"
+);
 export const DEV_DIR = join(process.cwd(), ".svelte-kit/output/client");
 export const CV_OUTPUT_DIR = join(process.cwd(), "cv-output");
