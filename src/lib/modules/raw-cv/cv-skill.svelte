6b533af59d47372c2be5d55e2a6d821c0089af4e
Max Kasperowicz <yet3.dev@gmail.com>
2025-10-06 16:24:11 +0200

    feat: make cv-skill text smaller

--- src/lib/modules/raw-cv/cv-skill.svelte
+++ src/lib/modules/raw-cv/cv-skill.svelte
@@ -7,4 +7,4 @@
 	const { label, class: className }: IProps = $props();
 </script>
 
-<li class={["py-1 px-2 rounded bg-base-240 w-fit text-[12px]", className]}>{label}</li>
+<li class={["py-1 px-2 rounded bg-base-240 w-fit text-[11px]", className]}>{label}</li>
