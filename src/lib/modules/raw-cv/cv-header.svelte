c0a51df434bad2150c208f61de21f4c2f46a7ea9
Max Kasperowicz <yet3.dev@gmail.com>
2025-10-06 16:33:12 +0200

    feat: make cv-header text smaller

--- src/lib/modules/raw-cv/cv-header.svelte
+++ src/lib/modules/raw-cv/cv-header.svelte
@@ -1,4 +1,4 @@
 <div>
-	<h1 class="font-medium text-[36px]">Maksymlian Kaspeorwicz</h1>
-	<h2 class="text-accent font-medium text-[26px]">Full-Stack Developer</h2>
+	<h1 class="font-medium text-[32px]">Maksymlian Kaspeorwicz</h1>
+	<h2 class="text-accent font-medium text-[24px]">Full-Stack Developer</h2>
 </div>
