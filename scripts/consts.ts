bff7f05eec0bb939c52f80f53f2b6f37b382d003
Max Kasperowicz <yet3.dev@gmail.com>
2025-10-08 19:54:08 +0200

    fix: build dir

--- scripts/consts.ts
+++ scripts/consts.ts
@@ -2,7 +2,6 @@ import { join } from "path";
 
 export const BUILD_DIR = join(
 	process.cwd(),
-	import.meta.env.VERCEL ? ".vercel/output/static" : "build",
 	process.env.VERCEL ? ".vercel/output/static" : "build"
 );
 export const DEV_DIR = join(process.cwd(), ".svelte-kit/output/client");
