5491b5b729ac6c8753f3478e0507caf102d99bf7
Max Kasperowicz <yet3.dev@gmail.com>
2025-11-02 20:54:04 +0100

    feat: change default particles' fps to 60

--- src/lib/modules/particles/make-default-paritcles-settings.ts
+++ src/lib/modules/particles/make-default-paritcles-settings.ts
@@ -2,7 +2,7 @@ import type { IParticlesSettings } from "$types/particles.types";
 
 export const makeDefaultParitclesSettings = (): IParticlesSettings => ({
 	isEnabled: true,
-  fps: 24,
+  fps: 60,
 	pixelation: 3,
 	spawnEvery: 1.3,
 });
