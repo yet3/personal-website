c8cc44f935588e842ad54914536019acadf95538
Max Kasperowicz <yet3.dev@gmail.com>
2025-11-18 22:33:58 +0100

    fix: typos, improve text

--- src/experiences/dentica.ts
+++ src/experiences/dentica.ts
@@ -30,6 +30,6 @@ export default defineExperience("Dentica", {
 	skills: SKILLS,
 	contentBlocks: [
 		"Collaborated closely with the dentist to create an easy-to-use and efficient system.",
-		"Developed multiple iterations of a patient management desktop app for a dentist. Built a user-friendly system for scheduling appointments and keeping track of patient procedures. Created an integration with an SMS API (using cloud functions) to send automated notifications and reminders to patients about their appointments, as well as to update dentist's schedules in real-time.",
+		"Developed multiple iterations of a patient management desktop app for a dentist. Built a user-friendly system for scheduling appointments and keeping track of patient procedures. Created an integration with an SMS API (using cloud functions) to send automated notifications and reminders to patients about their appointments, as well as to update dentist schedules in real-time.",
 	],
 });
--- src/experiences/storydoc.ts
+++ src/experiences/storydoc.ts
@@ -34,6 +34,6 @@ export default defineExperience("Storydoc", {
 	contentBlocks: [
 		"Collaborated with an international team of developers, ui/ux/story designers and product managers.",
 		"Worked on an AI-powered presentation SaaS. Implemented features such as a fully interactive WYSIWYG rich text editor, AI integration, new slide components, and PDF generation using Puppeteer. Focused on optimizing the product and was responsible for managing bug fixing.",
-		"Worked on internal tooling for testing/adjusting ai features",
+		"Worked on internal tooling for testing/adjusting AI features",
 	],
 });
