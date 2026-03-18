831ccd1de81a8ed456f28823d16941cf34eb9ac6
Max Kasperowicz <yet3.dev@gmail.com>
2026-03-18 03:06:07 +0100

    feat: change the look of experience dates

--- apps/web/src/pages/_components/experience/experience-dates.svelte
+++ apps/web/src/pages/_components/experience/experience-dates.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import {
-    formatExperienceDates,
-    type IExperience,
-  } from "@repo/content"
+  import { formatExperienceDates, type IExperience } from "@repo/content";
 
   interface IProps {
     class?: string;
@@ -11,22 +8,17 @@
   const { class: className, dates }: IProps = $props();
 </script>
 
-<code
-  class={[
-    "text-secondary flex w-fit items-start sm:items-center font-medium max-sm:flex-col",
-    className,
-  ]}
->
-  [
-  <ol class="flex max-sm:flex-col">
-    {#each dates as date, idx}
-      <li class="inline-flex max-sm:ml-2">
+<code class={["text-secondary flex w-fit items-center font-medium", className]}>
+  <ol class="grid content-start gap-0.5">
+    {#each [...dates].sort((a, b) => a[0].getTime() - b[0].getTime()) as date, idx}
+      <li class="inline-flex">
+        <span>{"<"}</span>
         {formatExperienceDates(date[0], date[1])}
+        <span>{">"}</span>
         {#if idx < dates.length - 1}
           <span class="mr-[1ch]">,</span>
         {/if}
       </li>
     {/each}
   </ol>
-  ]
 </code>
