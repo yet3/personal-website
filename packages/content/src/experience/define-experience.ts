9d788336567505f8def33ec36537253faf42d350
Max Kasperowicz <yet3.dev@gmail.com>
2026-06-21 22:35:27 +0200

    feat: sort experience dates

--- packages/content/src/experience/define-experience.ts
+++ packages/content/src/experience/define-experience.ts
@@ -8,5 +8,6 @@ export const defineExperience = <TAt extends string>(
 		[at]: {
 			at: at,
 			...d,
+      dates: [...d.dates].sort((a,b) => a[0].getTime() - b[0].getTime()),
 		} as IExperience,
 	}) as Record<TAt, IExperience>;
