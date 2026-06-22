05e526cf524f6449772366e3d160d2437b00653d
Max Kasperowicz <yet3.dev@gmail.com>
2026-06-22 16:09:48 +0200

    refactor: remove unsued styles

--- apps/cv/uno.config.ts
+++ apps/cv/uno.config.ts
@@ -16,24 +16,8 @@ export default mergeConfigs([
       (theme) => {
         return {
           ...theme,
-          spacing: {
-            ...theme.spacing,
-            columns: "var(--columns-gap)",
-            sections: "var(--sections-gap)",
-            skills: "var(--skills-gap)",
-          },
           text: {
             ...theme.text,
-            h1: makeSize("size-h1"),
-            h2: makeSize("size-h2"),
-            "head-info": makeSize("size-head-info"),
-            //
-            skill: makeSize("size-skill"),
-            //
-            content: makeSize("size-content", "1.5"),
-            "sec-h1": makeSize("size-sec-h1"),
-            "sec-h2": makeSize("size-sec-h2"),
-            "sec-h3": makeSize("size-sec-h3"),
             //
             "resume-name": makeSize("size-resume-name"),
             "resume-subname": makeSize("size-resume-subname"),
